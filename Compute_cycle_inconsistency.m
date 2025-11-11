beabd96bd3129bc3470b2ec2b9f0080773565b36
sli743 <54452671+sli743@users.noreply.github.com>
2025-11-10 22:27:24 -0600

    Update Compute_cycle_inconsistency.m

--- Compute_cycle_inconsistency.m
+++ Compute_cycle_inconsistency.m
@@ -1,5 +1,19 @@
-%% Author: Shaohan Li
-%% © Regents of the University of Minnesota. All rights reserved
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright & License Notice
+%% Cycle-Sync is copyrighted by Regents of the University of Minnesota and
+%% the Regents of the University of California and covered by US 63/903,432. 
+%% Regents of the University of Minnesota and the Regents of the University
+%% of California will license the use of <Software Name> solely for 
+%% educational and research purposes by non-profit institutions and US 
+%% government agencies only. For other proposed uses, contact umotc@umn.edu.
+%% The software may not be sold or redistributed without prior approval. 
+%% One may make copies of the software for their use provided that the 
+%% copies, are not sold or distributed, are used under the same terms and
+%% conditions. As unestablished research software, this code is provided on
+%% an "as is" basis without warranty of any kind, either expressed or implied.
+%% The downloading, or executing any part of this software constitutes an 
+%% implicit agreement to these terms. These terms and conditions are subject
+%% to change at any time without prior notice.
 %%------------------------------------------------
 %% Compute cycle inconsistency for translation vectors
 %%------------------------------------------------
@@ -86,4 +100,5 @@ function[out] = Compute_cycle_inconsistency(Ind,tijMat,nsample)
     out.CoIndMat = CoIndMat;
     out.IndPos = IndPos;
 return
+
 
