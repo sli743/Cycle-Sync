43fbc7f12a61130cdcadd27a3ff9c50052eb70b7
sli743 <54452671+sli743@users.noreply.github.com>
2025-11-10 22:26:39 -0600

    Update Cycle_Sync.m

--- Cycle_Sync.m
+++ Cycle_Sync.m
@@ -1,4 +1,20 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright & License Notice
+%% Cycle-Sync is copyrighted by Regents of the University of Minnesota and
+%% the Regents of the University of California and covered by US 63/903,432. 
+%% Regents of the University of Minnesota and the Regents of the University
+%% of California will license the use of <Software Name> solely for 
+%% educational and research purposes by non-profit institutions and US 
+%% government agencies only. For other proposed uses, contact umotc@umn.edu.
+%% The software may not be sold or redistributed without prior approval. 
+%% One may make copies of the software for their use provided that the 
+%% copies, are not sold or distributed, are used under the same terms and
+%% conditions. As unestablished research software, this code is provided on
+%% an "as is" basis without warranty of any kind, either expressed or implied.
+%% The downloading, or executing any part of this software constitutes an 
+%% implicit agreement to these terms. These terms and conditions are subject
+%% to change at any time without prior notice.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 %% Input: AdjMat (img_num by img_num adjacency matrix), 
 %% tijMat (3 by edge_num matrix that stores the pairwise directions),
@@ -251,6 +267,7 @@ else
 end
 
 return
+
 
 
 
